f32b9af606af553cee2acdb7352493d49cea7fee
Martin-J-Larre <martinjlarre@gmail.com>
2022-02-26 18:15:27 -0500

    4# middlewares folder created

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,32 +1,37 @@
+const { Router } = require('express');
 const express = require('express');
 const router = express.Router();
-const CryptoJS = require('crypto-js');
+// const CryptoJS = require('crypto-js');
 
 
-const User = require('../models/UserModel');
-const { verifyToken, verifyTokenAndAuthorization } = require('../controllers/userController');
+// const User = require('../models/UserModel');
+const { verifyToken, verifyTokenAndAuthorization } = require('../middlewares/auth');
+const { updateUser } = require('../controllers/userControllers');
 
-//------------ UPDATE
-router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
-    if (req.body.password) {
-        req.body.password = CryptoJS.AES.encrypt(
-            req.body.password,
-            process.env.PASS_SEC
-        ).toString();
-    }
+router.put('/:id', verifyTokenAndAuthorization, updateUser);
 
-    try {
-        const updatedUser = await User.findByIdAndUpdate(
-            req.params.id,
-            {
-                $set: req.body,
-            },
-            { new: true }
-        );
-        res.status(200).json(updatedUser);
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
+
+//------------ OLD UPDATE DELETE AFTER
+// router.put('/:id', verifyTokenAndAuthorization, async (req, res) => {
+//     if (req.body.password) {
+//         req.body.password = CryptoJS.AES.encrypt(
+//             req.body.password,
+//             process.env.PASS_SEC
+//         ).toString();
+//     }
+
+//     try {
+//         const updatedUser = await User.findByIdAndUpdate(
+//             req.params.id,
+//             {
+//                 $set: req.body,
+//             },
+//             { new: true }
+//         );
+//         res.status(200).json(updatedUser);
+//     } catch (err) {
+//         res.status(500).json(err);
+//     }
+// });
 
 module.exports = router;
