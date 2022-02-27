373e5fa77937e65c286aaa5d82673a813d0cf6b1
Martin-J-Larre <martinjlarre@gmail.com>
2022-02-27 01:49:08 -0500

    8# orders crud finished

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,6 +1,17 @@
+const { Router } = require('express');
 const express = require('express');
 const router = express.Router();
 
-// router.put('/id',)
+// // const { verifyToken, verifyTokenAndAuthorization, verifyTokenAndAdmin } = require('../middlewares/auth');
+//todo: Poner la auth con token cuando se ponga más serio
+const { createOrder, updateOrder, deleteOrder, getUserOrder, getAllOrders } = require('../controllers/orderController');
+
+
+router.post("/", createOrder);
+router.get("/", getAllOrders);
+router.get("/find/:userId", getUserOrder);
+router.put("/:id", updateOrder);
+router.delete("/:id", deleteOrder);
 
 module.exports = router;
+
